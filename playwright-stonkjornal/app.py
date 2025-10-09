29d8bf2c1d7916faed4602f84cb01655fe9ec673
Omer Elezra <omer.elezra@intel.com>
2025-10-09 18:02:04 +0300

    aa

--- playwright-stonkjornal/app.py
+++ playwright-stonkjornal/app.py
@@ -101,6 +101,7 @@ async def ingest(request: Request):
             f"--username {shlex.quote(username)} "
             f"--password {shlex.quote(password)} "
             f"--csv-file {shlex.quote(csv_path)}"
+            f"--headful"
         )
         
         logger.info(f"Executing automation script for {filename}")
